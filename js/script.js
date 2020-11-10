150565ed5e2b601644da765eb858862f390a8bcd
Michael DeGori <michael.degori@gmail.com>
2020-11-10 09:05:01 -0500

    smooth animations of sprites

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = window.innerWidth * .97
-canvas.height = canvas.width * .6
+let width = canvas.width = 800
+let height = canvas.height = 450
 
 const bgImg = new Image()
 bgImg.src = './images/BG/BG.png'
 
 const dinoImg = new Image()
-dinoImg.src = `./images/dino-sprites/spritesheet.png`
+dinoImg.src = `./images/dino-sprites/dino-sprite.png`
 
 
-let bg = { x: 0, y: 0, w: canvas.width, h: canvas.height }
+let bg = { x: 0, y: 0, w: width, h: height }
 
 let dino = {
     x: 0,
     y: 0,
-    w: 100,
-    h: 100,
-    // y: canvas.height * .8,
-    // w: canvas.width,
-    // h: canvas.height * .2,
+    w: 80,
+    h: 80,
+    // y: height * .8,
+    // w: width,
+    // h: height * .2,
     speed: 3,
     velX: 0,
     velY: 0,
@@ -29,7 +29,7 @@ let dino = {
     idling: true,
     running: false,
     dying: false,
-    // rightFacing: true
+    rightFacing: true
 }
 
 let dinoColPts = {
@@ -48,14 +48,14 @@ let boxes = [];
 // BOTTOM BOUNDARY
 boxes.push({
     x: 0,
-    y: canvas.height,
-    w: canvas.width,
+    y: height,
+    w: width,
     h: 5
 });
 
 boxes.push({
-    x: canvas.width / 2,
-    y: canvas.height - 50,
+    x: width / 2,
+    y: height - 50,
     w: 200,
     h: 50
 })
@@ -132,6 +132,11 @@ function update() {
 
     if (keys[38] || keys[32]) {
         // up arrow or space
+        if (dino.rightFacing == true) {
+            changeAction('jumpRight')
+        } else {
+            changeAction('jumpLeft')
+        }
         if (!dino.jumping && dino.grounded) {
             dino.jumping = true;
             dino.grounded = false;
@@ -145,7 +150,6 @@ function update() {
     if (keys[39]) {
         // right arrow
         changeAction('runRight')
-
         if (dino.velX < dino.speed) {
             dino.velX++;
         }
@@ -157,8 +161,6 @@ function update() {
     if (keys[37]) {
         // left arrow
         changeAction('runLeft');
-
-
         if (dino.velX > -dino.speed) {
             dino.velX--;
         }
@@ -167,10 +169,6 @@ function update() {
         dino.running = true;
     }
 
-    // if (!dino.rightFacing) {
-    //     dino.x -= dino.w * .1;
-    // }
-
     dino.velX *= friction;
     dino.velY += gravity;
 
@@ -178,7 +176,7 @@ function update() {
     for (let i = 0; i < boxes.length; i++) {
         ctx.fillRect(boxes[i].x, boxes[i].y, boxes[i].w, boxes[i].h);
 
-        let side = collisionCheck(dino, dinoColPts, boxes[i]);
+        let side = collisionCheck(dino, boxes[i]);
         if (side === "l" || side === "r") {
             dino.velX = 0;
             dino.jumping = false;
@@ -198,12 +196,12 @@ function update() {
     dino.y += dino.velY;
 }
 
-function collisionCheck(character, collision, obstacle) {
+function collisionCheck(character, obstacle) {
     // get the vectors to check against
-    let vX = (character.x + (collision.w / 2)) - (obstacle.x + (obstacle.w / 2)),
-        vY = (character.y + (collision.h / 2)) - (obstacle.y + (obstacle.h / 2)),
+    let vX = (character.x + (character.w / 2)) - (obstacle.x + (obstacle.w / 2)),
+        vY = (character.y + (character.h / 2)) - (obstacle.y + (obstacle.h / 2)),
         // add the half widths and half heights of the objects
-        hWidths = (collision.w / 2) + (obstacle.w / 2),
+        hWidths = (character.w / 2) + (obstacle.w / 2),
         hHeights = (character.h / 2) + (obstacle.h / 2),
 
         collisionSide = null;
@@ -240,43 +238,43 @@ document.body.addEventListener("keydown", function (e) {
 
 document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
-    action = "idleRight"
+    if (dino.rightFacing == true) {
+        action = 'idleRight'
+    } else if (dino.rightFacing == false) {
+        action = 'idleLeft'
+    }
 });
 
 function animate() {
     animationId = requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, width, height);
     ctx.drawImage(bgImg, bg.x, bg.y, bg.w, bg.h)
     drawDino()
     update()
 }
 
 let stages = {
-    idleRight: { s: 0, w: 680, num: 10 },
-    idleLeft: { s: 7050, w: 680, num: 10 },
-    runRight: { s: 13630, w: 680, num: 8 },
-    runLeft: { s: 19270, w: 680, num: 8 }
+    idleRight: { s: -30, w: 680, num: 10 },
+    idleLeft: { s: 7085, w: 680, num: 10 },
+    runRight: { s: 13620, w: 680, num: 8 },
+    runLeft: { s: 19270, w: 680, num: 8 },
+    jumpRight: { s: 24500, w: 680, num: 10 },
+    jumpLeft: { s: 31510, w: 680, num: 10 },
+    deadRight: { s: 38080, w: 680, num: 8 },
+    deadLeft: { s: 43520, w: 680, num: 8 },
 }
 
-
-let action = "idleRight"
+let action = 'idleRight'
 let x = stages[action].s
 
 function drawDino() {
-    ctx.fillStyle = "blue"
-    ctx.fillRect(dino.x, dino.y, dino.w, dino.h)
-    ctx.drawImage(dinoImg, x, 0, 430, dinoImg.height, dino.x, dino.y, 100, 100)
+    // ctx.fillStyle = "blue"
+    // ctx.fillRect(dino.x, dino.y, dino.w, dino.h)
+    ctx.drawImage(dinoImg, x, 0, 430, dinoImg.height, dino.x, dino.y, dino.w, dino.h)
 }
 dinoImg.onload = animate;
 
-
-
 setInterval(function () {
-    // x += dinoImg.width / numImg
-    // if (x >= dinoImg.width) {
-    //     x = 0
-    // }
-    console.log(stages)
     x += stages[action].w
     if (x >= (stages[action].w * stages[action].num) + stages[action].s) {
         x = stages[action].s
